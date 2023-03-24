af79c0f94bf9a67c87136d9c0f2ba6a2903dee7a
methatox <methatox@icloud.com>
2023-03-24 23:41:24 +0100

    button change worked

---
+++ AddToCart/src/AddToCart.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace AddToCart;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class AddToCart extends Plugin implements ThemeInterface
+{
+}
--- AddToCartButton/src/AddToCartButton.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace AddToCartButton;
-
-use Shopware\Core\Framework\Plugin;
-
-class AddToCartButton extends Plugin
-{
-}
