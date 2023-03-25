74ea829d5c041ae62a223dd99b9f62d0827c374d
methatox <methatox@icloud.com>
2023-03-25 16:05:31 +0100

    commit before deletion

---
+++ Customized/src/Customized.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Customized;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Customized extends Plugin implements ThemeInterface
+{
+}
